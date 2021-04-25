45535b3ab23dbe53ccdb3ead0a716246c0a1330e
Thanhnt237 <tuanthanh9a22014@gmail.com>
2021-04-25 22:33:20 +0700

    training action

---
+++ model.js
@@ -0,0 +1,43 @@
+//normalize data
+var heights = [];
+var sizes = [];
+const sizeCategories = ["XXS","XS","S","M","L","XL","XXL","XXXL"];
+
+data.map(item => {
+  heights.push( (item.height - 1200)/900 );
+  sizes.push(sizeCategories.indexOf(item.size));
+});
+
+console.log(heights);
+//console.log(sizes);
+//console.log("max: ", Math.max.apply(Math, heights));
+//console.log("min: ", Math.min.apply(Math, heights));
+
+let inputdata = tf.tensor1d(heights);
+let sizesTensor = tf.tensor1d(sizes, "int32");
+let outputdata = tf.oneHot(sizesTensor, 8);
+
+inputdata.print();
+//outputdata.print();
+
+//create model
+let model = tf.sequential();
+
+let hiddenLayer = tf.layers.dense({
+  units: 16,
+  activation: "sigmoid",
+  inputDim: 1
+});
+
+let outputLayer = tf.layers.dense({
+  units: 8,
+  activation: "softmax"
+});
+
+model.add(hiddenLayer);
+model.add(outputLayer);
+
+model.compile({
+  optimizer: tf.train.sgd(0.2),
+  loss: "categoricalCrossentropy"
+});
---
+++ script.js
@@ -0,0 +1,37 @@
+const trainCount = 10
+
+//train model
+async function train(){
+  let options = {
+    epochs: trainCount,
+    validationSplit: 0.1,
+    shuffle: true
+  }
+  return await model.fit(inputdata,outputdata, options);
+};
+console.log("Train start");
+
+train().then((result)=>{
+  console.log("Train finished");
+  console.log(result);
+
+  let heightTextBox = document.getElementById("height");
+  let getResultBtn = document.getElementById("getResult");
+  let resultText = document.getElementById("resultText");
+
+  getResultBtn.addEventListener("click", ()=>{
+    let test = heightTextBox.value;
+    let heightInput = (test - 1200)/900;
+    let heightInputTensor = tf.tensor1d([heightInput]);
+
+    console.log(test + "+" + heightInput);
+
+    //Predict
+    let predictResult = model.predict(heightInputTensor);
+    let max = predictResult.argMax().dataSync();
+
+    console.log(max);
+    resultText.innerHTML = "size ao phu hop cua ban la: " + sizeCategories[max];
+  })
+
+});
